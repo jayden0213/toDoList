45afd85da6509f81695b7ef82c4eca01c0823e49
jayden <guitarlove00@gmail.com>
2020-05-01 19:29:12 +0900

    배경 넣기 코드 추가 중

--- bg.js
+++ bg.js
@@ -3,11 +3,16 @@ const body = document.querySelector("body");
 
 const IMG_NUMBER = 5;
 
+function paintImage() {
+
+    
+}
+
+
 function genRandom() {
     const number = Math.floor(Math.random() * IMG_NUMBER);
     return number;
 }
-
 
 function init() {
     const radomNumber = genRandom();
