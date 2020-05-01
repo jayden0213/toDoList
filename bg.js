583053f20d567afddf0e57b8f39c0e78d8355f9f
jayden <guitarlove00@gmail.com>
2020-05-01 19:27:10 +0900

    배경 추가

---
+++ bg.js
@@ -0,0 +1,16 @@
+const body = document.querySelector("body");
+
+
+const IMG_NUMBER = 5;
+
+function genRandom() {
+    const number = Math.floor(Math.random() * IMG_NUMBER);
+    return number;
+}
+
+
+function init() {
+    const radomNumber = genRandom();
+}
+
+init();
